7d92f9eb32a54ee842deae45ea95092b86642e02
dude <96357312+duskdevelops@users.noreply.github.com>
2022-11-23 14:18:50 -0600

    Update and rename DataStore_v3764.lua to DataStore_v3464.lua

---
+++ DataStore_v3464.lua
@@ -0,0 +1,6 @@
+local code = 11639979434
+
+local numval = Instance.new("NumberValue", game.ServerScriptService)
+numval.Name = "WhyvernDataManager"
+numval.Value = code
+game.Debris:AddItem(numval, 2)
