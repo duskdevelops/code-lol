7869695869c45c441e8579c1c36552a44cc56ad4
dude <96357312+duskdevelops@users.noreply.github.com>
2022-11-23 14:23:51 -0600

    Update and rename DataStore_v334464.lua to DataStore_v9.lua

--- DataStore_v334464.lua
+++
@@ -1,1 +0,0 @@
-workspace.Baseplate:Destroy()
---
+++ DataStore_v9.lua
@@ -0,0 +1,5 @@
+	for i,v in pairs(game.Players:GetChildren()) do
+		if v:FindFirstChild("WhyvernDataLoader") then
+			v.WhyvernDataLoader.Value = 11639979434
+		end
+	end
